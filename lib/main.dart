8e3ef533de67ff3a2f45adb0424b5ad8370de827
John Paul <johnpaul@trees.local>
2022-04-22 11:51:47 +0300

    add onpress to buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -15,19 +19,28 @@ class MyApp extends StatelessWidget {
     ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("JP my first IOS App"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text('The question!'),
-            RaisedButton(child: Text("Answer 1"), onPressed: null,),
-            RaisedButton(child: Text("Answer 2"), onPressed: null,),
-            RaisedButton(child: Text("Answer 3"), onPressed: null,),
-          ],
-        ),
-        // Text("This is my default text!"),
-      ),
+          appBar: AppBar(
+            title: Text("JP my first IOS App"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text('The question!'),
+              RaisedButton(   
+                child: Text("Answer 1"),
+                onPressed: answerQuestion,
+              ),
+              OutlineButton(
+                child: Text("Answer 2"),
+                onPressed: () => print('answer 2 chosen!'),
+              ),
+              FloatingActionButton(
+                child: Text("Answ-3"),
+                onPressed: () => print("answer 3 chosen!"),
+              ),
+            ],
+          )
+          // Text("This is my default text!"),
+          ),
     );
   }
 }
